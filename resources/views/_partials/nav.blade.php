8b5cf1f247c794c1b7d64e74d84d11ea658f0483
Vaggelis Yfantis <me@octoper.me>
2021-01-27 15:04:06 +0200

    some improvements

--- resources/views/_partials/nav.blade.php
+++ resources/views/_partials/nav.blade.php
@@ -1,4 +1,4 @@
-<header class="flex bg-white dark:bg-gray-700 transition-colors duration-500 ease-in-out items-center shadow-sm bg-white h-16 py-4" role="banner">
+<header class="flex bg-white dark:bg-gray-700 transition-all duration-500 ease-in-out items-center shadow-sm bg-white h-16 py-4" role="banner">
     <div class="container flex items-center max-w-8xl mx-auto px-4 space-x-4 lg:px-4">
         <div class="flex items-center lg:hidden">
             <button aria-label="Toggle Documentation Navigation" @click.prevent="toggle()">
