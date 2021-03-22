32ee027a0a7a19f455ae010d29d9e1a34cdd04e8
Nuno Maduro <enunomaduro@gmail.com>
2021-03-22 20:40:18 +0000

    Adds Meema as sponsor

--- resources/views/components/welcome/sponsors.blade.php
+++ resources/views/components/welcome/sponsors.blade.php
@@ -4,14 +4,14 @@
             <div class="flex flex-col lg:flex-row items-center space-y-4 lg:space-x-4 lg:space-y-0">
                 <div class="font-semibold text-gray-500">Sponsors:</div>
                 <div>
-                    <a href="https://scoutapm.com/" target="_blank" rel="noopener">
-                        <img alt="Scout APM" loading="lazy" style="max-width: 200px;" src="{{ asset('assets/img/sponsors/scout-apm.jpg') }}">
+                    <a href="https://akaunting.com/" target="_blank" rel="noopener">
+                        <img alt="Akaunting" loading="lazy" style="max-width: 200px;" src="{{ asset('assets/img/sponsors/akaunting-logo.svg') }}">
                         </img>
                     </a>
                 </div>
                 <div>
-                    <a href="https://akaunting.com/" target="_blank" rel="noopener">
-                        <img alt="Akaunting" loading="lazy" style="max-width: 200px;" src="{{ asset('assets/img/sponsors/akaunting-logo.svg') }}">
+                    <a href="https://meema.io/" target="_blank" rel="noopener">
+                        <img alt="Meema" loading="lazy" style="max-width: 150px;" src="{{ asset('assets/img/sponsors/meema.png') }}">
                         </img>
                     </a>
                 </div>
