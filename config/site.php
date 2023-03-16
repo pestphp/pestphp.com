59f6d823b1ebd5ac5781192ea892cbf92f5caaf2
Nuno Maduro <enunomaduro@gmail.com>
2023-03-16 22:58:46 +0000

    wip

--- config/site.php
+++ config/site.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => 'Pest',
-    'title' => 'Pest - An elegant PHP Testing Framework',
-    'description' => 'Introducing Pest: An elegant PHP Testing Framework with a focus on simplicity',
+    'title' => 'Pest - The elegant PHP Testing Framework',
+    'description' => 'Introducing Pest: The elegant PHP Testing Framework with a focus on simplicity',
     'discord' => 'https://discord.gg/bMAJv82',
     'twitter' => 'pestphp',
     'github' => 'https://github.com/pestphp/pest',
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,23 +4,34 @@
 <head>
     <title>{{ $title = empty($title) ? config('site.title') : "{$title} | ".config('site.title') }}</title>
     <meta charset="utf-8">
-    <meta name="author" content="Nuno Maduro">
-    <meta name="keywords" content="PHP, PHPUnit, Testing, Framework">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="{{ $description = $description ?? 'Pest is a Testing Framework with a focus on simplicity. It was carefully crafted to bring the joy of testing to PHP.' }}">
+    <title>Pest | The elegant PHP testing framework</title>
+    <link href="https://pestphp.com/" rel="canonical">
+
+    <meta name="twitter:site" content="@pestphp">
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:creator" content="@enunomaduro">
+    <meta name="twitter:creator" content="@pestphp">
     <meta name="twitter:image:alt" content="{{ $title }}">
+
+    <meta charset="UTF-8">
+    <meta name="author" content="Nuno Maduro">
+    <meta name="keywords" content="PHP, Pest, Testing Framework">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <meta name="description" content="{{ $description = $description ?? 'Pest is a testing framework with a focus on simplicity, meticulously designed to bring back the joy of testing in PHP.' }}">
+
     <meta property="og:type" content="website">
     <meta property="og:title" content="{{ $title }}">
     <meta property="og:description" content="{{ $description }}">
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:image" content="https://pestphp.com/www/og.png">
+
+    @if (str_ends_with(url()->current(), 'announcing-pest2'))
+        <meta property="og:image" content="https://next.pestphp.com/assets/img/og-announcement.png">
+    @else
+        <meta property="og:image" content="https://next.pestphp.com/assets/img/og.png">
+    @endif
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="icon" href="/www/favicon.svg" type="image/svg+xml">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">
 
     {{ $head ?? '' }}
 
