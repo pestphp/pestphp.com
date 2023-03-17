034934ba92032ce15ea7ed6aed64d2a1c13fdb3f
Francisco Madeira <xico2k@gmail.com>
2023-03-17 14:23:52 +0000

    fix: Change wrapper size.

--- resources/views/ide.blade.php
+++ resources/views/ide.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout layout="app" title='IDE plugins' description="Pest has IDE plugins for PhpStorm and VSCode.">
     <main role="main" class="w-full flex-auto">
         <section class="documentation">
-            <div class="max-w-8xl mx-auto px-4 lg:px-4 xl:px-6 lg:flex lg:flex-row">
+            <div class="max-w-5xl mx-auto px-4 lg:px-4 xl:px-6 lg:flex lg:flex-row">
 
                 <x-docs.sidebar>
                     {!! $index !!}
                 </x-docs.sidebar>
 
-                <div class="docs-main DocSearch-content lg:ml-10 xl:ml-6 lg:px-0 relative w-full px-3 mt-6 mb-20">
+                <div class="docs-main max-w-prose DocSearch-content mx-auto lg:mr-0 relative w-full px-3 lg:px-0 mt-6 mb-20">
                     <div class="w-100 lg:block absolute top-0 right-0 hidden h-8 mt-2 text-sm">
                         <a href="https://github.com/pestphp/pestphp.com-next/edit/main/resources/views/ide.blade.php">
                             Edit this page →
