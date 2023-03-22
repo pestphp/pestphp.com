39814d51606a6d65f0a797b861ab793603ec4348
Lucas Giovanny <lucasgiovanny@gmail.com>
2023-03-22 20:07:46 +0000

    Create redirects

---
+++ app/Http/Middleware/RedirectRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $path = optional(config('redirects.redirects'))->get($request->path());
+        
+        if($path){
+            return redirect()->to($path, config('redirects.status') ?? 301);
+        }
+
+        return $next($request);
+    }
+}
