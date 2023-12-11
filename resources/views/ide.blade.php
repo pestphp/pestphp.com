04e891aab79549512c8e3d671034704fe67f7ea7
Nuno Maduro <enunomaduro@gmail.com>
2023-12-11 10:36:08 +0000

    Fixes IDE edit

--- resources/views/ide.blade.php
+++ resources/views/ide.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="docs-main max-w-prose DocSearch-content mx-auto lg:mr-0 relative w-full px-3 lg:px-0 mt-6 mb-20">
                     <div class="w-100 lg:block absolute top-0 right-0 hidden h-8 mt-2 text-sm">
-                        <a href="https://github.com/pestphp/pestphp.com-next/edit/main/resources/views/ide.blade.php">
+                        <a href="https://github.com/pestphp/pestphp.com/edit/next/resources/views/ide.blade.php">
                             Edit this page →
                         </a>
                     </div>
