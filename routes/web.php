e0855e466487f03eac602bfb680efc8200beb0bd
nuno maduro <enunomaduro@gmail.com>
2026-04-14 07:24:29 -0700

    md

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::group([
 
     Route::get('/docs/editor-setup', IDEPluginsController::class)->name('ide-plugins');
     Route::get('/docs/{page}/llms.txt', [LlmsTxtController::class, 'page']);
+    Route::get('/docs/{page}.md', [LlmsTxtController::class, 'page']);
     Route::get('/docs/{page?}', DocsController::class)->name('docs')->where('page', '.*');
 });
 
