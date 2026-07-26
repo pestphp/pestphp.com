f535e5afe6347d08a40000f9ee1b4c936d97a0c7
Nuno Guerra <nunoandre360@gmail.com>
2026-07-26 22:18:27 +0100

    self-host the fonts, preload the critical woff2 files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
     x-data="offcanvasNav">
 
     <x-slot name="head">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="preload" href="/www/fonts/archivo-var.woff2" as="font" type="font/woff2" crossorigin>
+        <link rel="preload" href="/www/fonts/instrument-sans-var.woff2" as="font" type="font/woff2" crossorigin>
+        <link rel="preload" href="/www/fonts/jetbrains-mono-var.woff2" as="font" type="font/woff2" crossorigin>
         {{ $head ?? '' }}
     </x-slot>
 
